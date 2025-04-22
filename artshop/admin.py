0b534daa629491af18b74d7009c8658733dad2ae
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-21 20:51:07 -0700

    wip: registered Artwork model for admin

--- artshop/admin.py
+++ artshop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Artwork
 
 # Register your models here.
+
+@admin.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category', 'is_active', 'created_at']
+    list_filter = ['is_active', 'category']
+    search_fields = ['title', 'description']
