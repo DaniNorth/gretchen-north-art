806abe89d63b2c8c131955581ee67f20fc5c76ed
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-21 23:01:41 -0700

    wip: added home view to display artworks using template

--- artshop/views.py
+++ artshop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Artwork
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def home(request):
+    artworks = Artwork.objects.all()
+    return render(request, "artshop/home.html", {"artworks": artworks})
+
+def artwork_detail(request, pk):
+    art = get_object_or_404(Artwork, pk=pk)
+    return render(request, "artshop/artwork_detail.html", {"art": art})
