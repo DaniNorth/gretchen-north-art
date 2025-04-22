65a8d15c59acfc8f2e0d4ada4817be1248cba324
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-22 00:24:21 -0700

    wip: fixed the universal database config for local + Render to fix the build/deployment error

--- gretchen_north_art/settings.py
+++ gretchen_north_art/settings.py
@@ -87,17 +87,14 @@ WSGI_APPLICATION = 'gretchen_north_art.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
