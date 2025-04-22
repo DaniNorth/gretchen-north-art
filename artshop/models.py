b53f18e2ec93aefdd612ab1804952b56853f95cc
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-21 20:49:53 -0700

    wip: created model for artwork in the artshop

--- artshop/models.py
+++ artshop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Artwork(models.Model):
+    title = models.CharField(max_length=200)
+    image = CloudinaryField('image')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
