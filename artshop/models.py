72448d47d18378b1fc6a17a83f8fd0437f1d15d2
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-03 14:32:44 -0700

    wip: added customer profile model

--- artshop/models.py
+++ artshop/models.py
@@ -88,3 +88,17 @@ class WishlistItem(models.Model):
 
     def __str__(self):
         return f"{self.artwork.title} in {self.wishlist.title}"
+    
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=100, blank=True)
+    address = models.TextField(blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.full_name or self.user.username
