289257cb6219b21e8b94bfa9e8d13353d88aadbc
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-03 14:32:16 -0700

    wip: linked signal file logic to app

--- artshop/apps.py
+++ artshop/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ArtshopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'artshop'
+
+    def ready(self):
+        import artshop.signals
