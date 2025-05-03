26b042b20f345035b7bfc14ca4be4bade9cde3bf
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-03 14:31:51 -0700

    wip: updated login path

--- gretchen_north_art/urls.py
+++ gretchen_north_art/urls.py
@@ -21,7 +21,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("artshop.urls")),
-    path("accounts/login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),  # ← explicitly name it
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("accounts/", include("allauth.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
