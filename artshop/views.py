85f444cb3a86940c2c240c519affbbfca03357e5
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-03 13:51:27 -0700

    wip: added authentication for add to cart actions and to ensure items are not accidently bought multiple times since each item is unique.

--- artshop/views.py
+++ artshop/views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
-from .models import Artwork
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils import timezone
 
-# Create your views here.
+from .models import Artwork, Cart, CartItem
+
 def home(request):
     artworks = Artwork.objects.all()
+    # Clear expired holds for display
+    for art in artworks:
+        art.clear_hold_if_expired()
     return render(request, "artshop/home.html", {"artworks": artworks})
 
 def artwork_detail(request, pk):
     art = get_object_or_404(Artwork, pk=pk)
+    art.clear_hold_if_expired()
     return render(request, "artshop/artwork_detail.html", {"art": art})
+
+@login_required
+def add_to_cart(request, pk):
+    artwork = get_object_or_404(Artwork, pk=pk)
+    artwork.clear_hold_if_expired()
+
+    if artwork.is_sold:
+        messages.error(request, "This artwork has already been sold.")
+        return redirect("home")
+
+    if artwork.is_held and artwork.held_by != request.user:
+        messages.warning(request, "This artwork is currently on hold for another customer.")
+        return redirect("home")
+
+    # Get or create the user's cart
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+
+    # Add artwork to cart or refresh the hold
+    artwork.hold(request.user)
+    CartItem.objects.get_or_create(cart=cart, artwork=artwork)
+
+    messages.success(request, f"'{artwork.title}' has been added to your cart. You've got 15 minutes to check out.")
+    return redirect("home")
