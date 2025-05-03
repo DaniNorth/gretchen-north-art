ace0ea5d784bf110c1a2ed01ea4790e6fdc616bd
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-03 13:53:28 -0700

    wip: added wishlist model to allow full crud coponent for users to add items to a list to view later.

--- artshop/models.py
+++ artshop/models.py
@@ -70,3 +70,21 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.artwork.title} in {self.cart.user.username}'s cart"
+    
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlists')
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.user.username})"
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name='items')
+    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.artwork.title} in {self.wishlist.title}"
