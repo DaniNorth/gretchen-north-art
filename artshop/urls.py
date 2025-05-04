5f36ae95aba4941370b643c67139c5848f2e3902
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-03 23:31:47 -0700

    wip: added path for add to cart

--- artshop/urls.py
+++ artshop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("artwork/<int:pk>/", views.artwork_detail, name="artwork_detail"),
+    path("cart/add/<int:pk>/", views.add_to_cart, name="add_to_cart"),
 ]
