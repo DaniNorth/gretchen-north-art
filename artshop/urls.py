c61315c5aaccbd950cc6b75ed5147f09e912e5d0
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-05-04 20:15:16 -0700

    wip: added a checkout status page path

--- artshop/urls.py
+++ artshop/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("cart/add/<int:pk>/", views.add_to_cart, name="add_to_cart"),
     path("cart/", views.cart_view, name="cart"),
     path("checkout/", views.checkout_view, name="checkout"),
+    path("checkout/complete/", views.checkout_complete_view, name="checkout_complete")
 ]
